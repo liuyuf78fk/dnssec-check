1c64e2a33dce91cf45c5bcdb3457f452a837b4dd
Liu Yu <f78fk@live.com>
2025-07-05 15:04:15 +0900

    add LICENSE
    Signed-off-by: Liu Yu <f78fk@live.com>

--- dnssec-check.c
+++ dnssec-check.c
@@ -1,3 +1,19 @@
+/*
+ * dnssec-check - Simple DNSSEC validation tool
+ * Copyright (C) 2025 Liu Yu
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY...
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
